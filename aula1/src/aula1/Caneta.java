7e3f453db755ae4068271a5332587275cf36497f
unknown <andrelopessfla@gmail.com>
2020-05-24 23:34:40 -0300

    aula2 configurando Visibilidade de atributo e métodos

--- aula1/src/aula1/Caneta.java
+++ aula1/src/aula1/Caneta.java
@@ -1,13 +1,13 @@
 package aula1;
 
 public class Caneta {
-	String modelo;
-	String cor;
-	float ponta;
-	int carga;
-	boolean tampada;
+    public String modelo;
+	public String cor;
+	private float ponta;
+	protected int carga;
+	private boolean tampada;
 	
-	void status() {
+	public void status() {
 		System.out.println("Modelo " + this.modelo);
 		System.out.println("Uma caneta " + this.cor);
 		System.out.println("Esta tampada ? " + this.tampada);
@@ -16,7 +16,7 @@ public class Caneta {
 		
 	}
 	
-	void rabiscar() {
+	public void rabiscar() {
 		if(this.tampada == true) {
 			System.out.println("Erro nao posso Rabiscar");
 			
@@ -27,12 +27,12 @@ public class Caneta {
 		
 	}
 
-	void tampar() {
+	public void tampar() {
 		this.tampada = true;
 		
 	}
 	
-	void destampar() {
+	public void destampar() {
 		this.tampada = false;
 		
 	}
--- aula1/src/aula1/aula1.java
+++ aula1/src/aula1/aula1.java
@@ -3,23 +3,11 @@ package aula1;
 public class aula1 {
 	public static void main(String[] args) {
 		Caneta c1 = new Caneta();
-		c1.modelo ="Bic";
+		c1.modelo ="BIC cristal";
 		c1.cor = "Azul";
-		c1.ponta = 0.5f;
-		c1.tampada = true;
-		c1.carga = 100;
+		c1.carga = 80;
+		c1.destampar();
 		c1.status();
-		c1.tampar();
-		c1.rabiscar();
-		Caneta c2 = new Caneta();
-		c2.modelo = "Bic";
-		c2.cor = "vermelho";
-		c2.ponta = 0.5f;
-		c2.tampada = true;
-		c2.destampar();
-		c2.status();
-		c2.rabiscar();
-		
 	}
 
 }
